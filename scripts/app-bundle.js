d477c5b95e789d268cd9d73fda42e2cd0205ac26
Andy Smith <pmsmith4@ncsu.edu>
2021-04-03 13:51:52 -0400

    no message

--- scripts/app-bundle.js
+++ scripts/app-bundle.js
@@ -5,32 +5,22 @@ define('app',['exports', 'aurelia-router'], function (exports, _aureliaRouter) {
     value: true
   });
   exports.App = undefined;
-
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
-  var App = exports.App = function () {
-    function App() {
-      _classCallCheck(this, App);
-
+  class App {
+    constructor() {
       this.message = 'Hello World!';
     }
 
-    App.prototype.attached = function attached() {};
+    attached() {}
 
-    App.prototype.configureRouter = function configureRouter(config, router) {
+    configureRouter(config, router) {
       config.title = 'MapTest';
 
       config.map([{ route: ['', 'map'], name: 'map', moduleId: './map', nav: true, title: 'MapTest' }]);
 
       this.router = router;
-    };
-
-    return App;
-  }();
+    }
+  }
+  exports.App = App;
 });
 define('environment',["exports"], function (exports) {
   "use strict";
@@ -70,9 +60,7 @@ define('main',['exports', './environment'], function (exports, _environment) {
       aurelia.use.plugin('aurelia-testing');
     }
 
-    aurelia.start().then(function () {
-      return aurelia.setRoot();
-    });
+    aurelia.start().then(() => aurelia.setRoot());
   }
 });
 define('map',["exports"], function (exports) {
@@ -81,37 +69,27 @@ define('map',["exports"], function (exports) {
   Object.defineProperty(exports, "__esModule", {
     value: true
   });
-
-  function _classCallCheck(instance, Constructor) {
-    if (!(instance instanceof Constructor)) {
-      throw new TypeError("Cannot call a class as a function");
-    }
-  }
-
   var myApp;
 
-  var Map = exports.Map = function () {
-    function Map() {
-      _classCallCheck(this, Map);
-    }
+  class Map {
 
-    Map.prototype.attached = function attached() {
+    attached() {
       myApp = this;
       var Loader = google["maps"].plugins["loader"].Loader;
 
-      var loader = new Loader({
+      const loader = new Loader({
         apiKey: "AIzaSyBzkzafS495xLhD6oS6qDUZ0bhu9YQKuDc",
         version: "weekly"
       });
-      loader.load().then(function () {
+      loader.load().then(() => {
         myApp.map = new google.maps.Map(document.getElementById("map"), {
           center: { lat: -34.397, lng: 150.644 },
           zoom: 8
         });
       });
-    };
+    }
 
-    Map.prototype.loadGoogleKML = function loadGoogleKML() {
+    loadGoogleKML() {
       var src = 'https://developers.google.com/maps/documentation/javascript/examples/kml/westcampus.kml';
 
       var kmlLayer = new google.maps.KmlLayer(src, {
@@ -124,9 +102,13 @@ define('map',["exports"], function (exports) {
         var testimonial = document.getElementById('capture');
         testimonial.innerHTML = content;
       });
-    };
+    }
 
-    Map.prototype.loadRobKML = function loadRobKML() {
+    loadRobKML() {
+      //var src = 'https://papaspuds.com/map-simple/bike-routes.kml';
+      //var src = "https://docs.google.com/document/d/18sQz-nrFyrQb7KM4FdVyqt78FwJQ9klAJWFfgTw_GRU/export?format="
+      //var src = "https://www.dropbox.com/s/15uznvw3da7o6gx/westcampus.kml"
+      // var src = "https://drive.google.com//0/uc?id=1gYxE4TL_R1aKf2bW7s40wzyBKt3S044P&export=download"
       var src = "https://raw.githubusercontent.com/pmsmith4/GoogleMapsAPITesting/main/bike-routes.kml";
 
       var kmlLayer = new google.maps.KmlLayer(src, {
@@ -141,10 +123,10 @@ define('map',["exports"], function (exports) {
       });
 
       console.log("Here");
-    };
+    }
 
-    return Map;
-  }();
+  }
+  exports.Map = Map;
 });
 define('resources/index',["exports"], function (exports) {
   "use strict";
@@ -153,7 +135,9 @@ define('resources/index',["exports"], function (exports) {
     value: true
   });
   exports.configure = configure;
-  function configure(config) {}
+  function configure(config) {
+    //config.globalResources([]);
+  }
 });
 define('text!app.html', ['module'], function(module) { module.exports = "<template><router-view></router-view></template>"; });
 define('text!map.html', ['module'], function(module) { module.exports = "<template><head><meta name=\"viewport\" content=\"initial-scale=1,user-scalable=no\"><meta charset=\"utf-8\"><title>KML Click Capture Sample</title><style>body,html{height:370px;padding:0;margin:0}#map{height:300px;width:480px;overflow:hidden;float:left;border:thin solid #333}#capture{height:360px;width:480px;overflow:hidden;float:left;background-color:#ececfb;border:thin solid #333;border-left:none}</style></head><body><table><tr><td><div id=\"map\"></div></td><td><div id=\"capture\"></div></td></tr><tr><td><button type=\"button\" id=\"loadGoogleKMLButton\" click.trigger=\"loadGoogleKML()\" style=\"margin-right:20px\">Load Google KML</button> <button type=\"button\" id=\"loadRobKMLButton\" click.trigger=\"loadRobKML()\" style=\"margin-right:20px\">Load Rob KML</button></td></tr></table></body></template>"; });
